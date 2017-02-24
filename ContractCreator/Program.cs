24a6f5d1fc9609905cdd3505e6de84708e8452c3
Aram Koukia <aramkoukia@gmail.com>
2017-02-23 21:58:48 -0800

    added the ContractCreator project and nuget packages

---
+++ ContractCreator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create one topic for each active node in the database
+            // Each node when they start will subsribe to a topic that matches their public key 
+
+            // pick random nodes and post CreateContact messages to the related topic
+
+        }
+    }
+}

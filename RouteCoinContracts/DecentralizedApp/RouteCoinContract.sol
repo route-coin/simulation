4f38b651fc9e1813a9e9e2a2dd683e9a479f83fa
Aram Koukia <aramkoukia@gmail.com>
2017-02-27 09:43:18 -0800

    added the call to getHupCount to increment the parent contract hupcount

--- RouteCoinContracts/DecentralizedApp/RouteCoinContract.sol
+++ RouteCoinContracts/DecentralizedApp/RouteCoinContract.sol
@@ -7,7 +7,7 @@ contract RouteCoin {
 
 	// this shows how many hups has happened until this contract was created.
 	// for now we will limit this to be 5 hups. after 5 hups any nodes getting a whisper about a contract, will ignore.
-	unit public hupCount;
+	uint hupCount;
 		
     // The public key of the buyer. Reza: we need to hash this.
     address buyer;   
@@ -27,7 +27,7 @@ contract RouteCoin {
     enum State { Created, Expired, Completed, Aborted, RouteFound }
     State state;
 
-    function RouteCoin(address _finalDestination, uint _contractGracePeriod, address _parentContract) 
+    function RouteCoin(address _finalDestination, uint _contractGracePeriod, address _parentContract, uint _hupCount) 
         payable
     {
         buyer = msg.sender;
@@ -35,6 +35,15 @@ contract RouteCoin {
         finalDestination = _finalDestination;
         contractGracePeriod = _contractGracePeriod;
 		parentContract = _parentContract;
+        if(_parentContract != address(0x0))
+        {
+            RouteCoin m = RouteCoin(_parentContract);
+            hupCount = m.getHupCount() + 1; 
+        }
+		else
+		{
+			hupCount = 0; 
+		}
     }
 
     modifier require(bool _condition) {
@@ -64,7 +73,7 @@ contract RouteCoin {
 
     function destinationAddressRouteFound()
         //expired // contract must be in the Created state to be able to foundDestinationAddress
-        //inState(State.Created)
+        inState(State.Created)
         returns (State)
     {
         seller = msg.sender;
@@ -75,7 +84,7 @@ contract RouteCoin {
 
     function destinationAddressRouteConfirmed()
         //onlyBuyer  // only buyer can confirm the working route 
-        //inState(State.RouteFound)  // contract must be in the Created state to be able to confirmPurchase
+        inState(State.RouteFound)  // contract must be in the Created state to be able to confirmPurchase
         payable
         returns (State)
     {
@@ -112,6 +121,12 @@ contract RouteCoin {
 	 	constant returns (address)
      {
          return parentContract;
+     }
+
+     function getHupCount()
+	 	constant returns (uint)
+     {
+         return hupCount;
      }
 
 
